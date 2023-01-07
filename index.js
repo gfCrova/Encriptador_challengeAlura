9bcdfb94acedede38dc0fe26fa6878889a71d2cb
gfCrova <giancrova.cl5@gmail.com>
2023-01-06 22:14:23 -0300

    comenzando con la funcion encriptar

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 
+const imagen = document.getElementById("div_imagen"); 
+const mensaje = document.getElementById("div_mensaje");
+mensaje.style.visibility = "hidden";
+imagen.style.visibility = "visible";
 
-const div = document.getElementById("div_mensaje");
-div.style.display="none";
+const encriptar = () => {
 
-const f = () => {
-    
+    const texto = document.getElementById("palabra").value;
+
+    mensaje.style.visibility = "visible";
+    mensaje.innerHTML = texto;
+    imagen.style.visibility = "hidden";
 };
