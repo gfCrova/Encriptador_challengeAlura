acc89baf88ce68d55cdb20edca2477c0e3ac8e7b
gfCrova <giancrova.cl5@gmail.com>
2023-01-07 16:03:43 -0300

    funciones encrip. y desencr. en marcha

--- index.js
+++ index.js
@@ -2,14 +2,32 @@
 
 const imagen = document.getElementById("div_imagen"); 
 const mensaje = document.getElementById("div_mensaje");
-mensaje.style.visibility = "hidden";
-imagen.style.visibility = "visible";
+
+const display = (a, b) => {
+    mensaje.style.visibility = a;
+    imagen.style.visibility = b;
+}
+display("hidden", "visible");
+
 
 const encriptar = () => {
 
+    display("visible", "hidden");
+
     const texto = document.getElementById("palabra").value;
-    const str = texto.replaceAll('e', 'enter');
-    imagen.style.visibility = "hidden";
-    mensaje.style.visibility = "visible";
-    mensaje.innerHTML = str;
+    const cifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+
+    mensaje.innerHTML = cifrado;
+};
+
+
+
+const desencriptar = () => {
+
+    display("visible", "hidden");
+
+    const texto = document.getElementById("palabra").value;
+    const cifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+
+    mensaje.innerHTML = cifrado;
 };
