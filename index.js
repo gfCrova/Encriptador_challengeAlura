dacf7ec4dd64ded26d65100813f4c1d157d923f3
gfCrova <giancrova.cl5@gmail.com>
2023-01-07 20:49:52 -0300

    nuevas funciones copiar y alert confirm

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 
 
 const imagen = document.getElementById("div_imagen"); 
-const mensaje = document.getElementById("div_mensaje");
+const mensaje = document.getElementById("mensaje");
+const button = document.getElementById("btnCopy");
 
-const display = (a, b) => {
+const display = (a, b, c) => {
     mensaje.style.visibility = a;
     imagen.style.visibility = b;
+    button.style.visibility = c;
 }
-display("hidden", "visible");
+display("hidden", "visible", "hidden");
 
 
 const encriptar = () => {
 
-    display("visible", "hidden");
+    display("visible", "hidden", "visible");
 
     const texto = document.getElementById("palabra").value;
     const cifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
@@ -24,10 +26,36 @@ const encriptar = () => {
 
 const desencriptar = () => {
 
-    display("visible", "hidden");
+    display("visible", "hidden", "visible");
 
     const texto = document.getElementById("palabra").value;
     const cifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
 
     mensaje.innerHTML = cifrado;
 };
+
+
+const copyText = () => {
+
+    mensaje.select();
+    mensaje.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(mensaje.value);
+
+    alertCopiado('Copiado', 'success')
+}
+
+
+const alertCopiado = (mensaje, colorCss) => {
+
+    const div = document.createElement('div');
+    div.className = `alert alert-${colorCss} position-absolute bi bi-check`;
+    div.appendChild(document.createTextNode(mensaje));
+
+    const container = document.querySelector('.div_mensaje');
+    const area = document.querySelector('#btnCopy');
+    container.insertBefore(div, area);
+
+    setTimeout(() => {
+        document.querySelector('.alert').remove();
+    }, 2500);
+}
