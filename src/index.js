f0156bf0bae422daa89464ba13cbfe302b530396
gfCrova <giancrova.cl5@gmail.com>
2023-01-09 15:15:35 -0300

    nueva carpeta src

---
+++ src/index.js
@@ -0,0 +1,90 @@
+
+const imagen = document.getElementById("div_imagen"); 
+const mensaje = document.getElementById("mensaje");
+const button = document.getElementById("btnCopy");
+
+const display = (a, b, c) => {
+    mensaje.style.visibility = a;
+    imagen.style.visibility = b;
+    button.style.visibility = c;
+}
+display("hidden", "visible", "hidden");
+
+
+// Funciones de Encriptación y Desescriptación del texto
+const encriptar = () => {
+
+    display("visible", "hidden", "visible");
+
+    const texto = document.getElementById("palabra").value;
+    const cifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    mensaje.innerHTML = cifrado;
+};
+
+const desencriptar = () => {
+
+    display("visible", "hidden", "visible");
+
+    const texto = document.getElementById("palabra").value;
+    const cifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    mensaje.innerHTML = cifrado;
+};
+
+
+// Función para COPIAR el texto
+const copyText = () => {
+
+    mensaje.select();
+    mensaje.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(mensaje.value);
+
+    alerts('Copiado', 'success', 2500, '.div_mensaje', '#btnCopy');
+}
+
+// Controlar el ingreso del texto (Solo Minúsculas y sin caracteres especiales)
+const check = (e) => {
+
+    tecla = (document.all) ? e.keyCode : e.which;
+    if (tecla == 8) {
+        return true;
+    }
+    patron = /[a-z\s]/;
+    tecla_final = String.fromCharCode(tecla);
+    if (!patron.test(tecla_final)) {
+        alerts('Solo letras minúsculas y sin acentos', 'danger', 1000, '.div_palabra', '.small');
+        disableTextArea();
+    }
+    return patron.test(tecla_final);
+}
+
+// Alerta de Contenido COPIADO
+const alerts = (mensaje, colorCss, time, contain, area) => {
+
+    const div = document.createElement('div');
+    div.className = `alert alert-${colorCss} position-absolute bi bi-check`;
+    div.appendChild(document.createTextNode(mensaje));
+
+    const container = document.querySelector(contain);
+    const textarea = document.querySelector(area);
+    container.insertBefore(div, textarea);
+
+    setTimeout(() => {
+        document.querySelector('.alert').remove();
+    }, time);
+}
+
+/*const alertChars = (msj, colorCss) => {
+    const div = document.createElement('div');
+    div.className = `alert alert-${colorCss} position-absolute bi bi-check`;
+    div.appendChild(document.createTextNode(msj));
+    div.insertBefore(textarea1);
+};*/  
+
+// Disable TEXTAREA
+const disableTextArea = () => {
+    document.getElementById("palabra").disabled = true;
+    setTimeout(() => {
+        document.getElementById("palabra").disabled = false;
+    }, 1000);
+}
+
